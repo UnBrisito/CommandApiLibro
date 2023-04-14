7c4570ecf427ea22b97f05f30d82dc201c4590e8
Álvaro Arias <alvaroariasmartinez@gmail.com>
2023-04-14 12:20:06 +0200

    Usamos secrets.json (credenciales editadas en sql)

--- src/Program.cs
+++ src/Program.cs
@@ -1,16 +1,21 @@
 using CommandsApi.Data;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 //public IConfiguration Configuration { get; } esto ya no funciona
 
 var builder = WebApplication.CreateBuilder(args);
+//Construir la cadena de conexión con los datos de secrets.json
+var strBuilder = new SqlConnectionStringBuilder();
+strBuilder.ConnectionString = builder.Configuration.GetConnectionString("SqlServerConnection");
+strBuilder.UserID = builder.Configuration["userId"];
+strBuilder.Password = builder.Configuration["password"];
 
 
-
-builder.Services.AddDbContext<ComandoContext>(opt => opt.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerConnection")));
+builder.Services.AddDbContext<ComandoContext>(opt => opt.UseSqlServer(strBuilder.ConnectionString));
 builder.Services.AddControllers();
-//builder.Services.AddScoped<ICommandsAPIRepo, MockCommandAPIRepo>();
+//builder.Services.AddScoped<ICommandsAPIRepo, MockCommandAPIRepo>(); //La implementación falsa
 builder.Services.AddScoped<ICommandsAPIRepo, ComandoEnSql>();
 
 var app = builder.Build();
